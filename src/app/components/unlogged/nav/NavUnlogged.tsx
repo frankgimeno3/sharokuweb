1e7578ad6063211f0e2f7f08a96171ea2e350edf
frank gimeno <frankgimeno3@gmail.com>
2024-04-16 18:22:55 +0200

    añado desplegable idioma

--- src/app/components/unlogged/nav/NavUnlogged.tsx
+++ src/app/components/unlogged/nav/NavUnlogged.tsx
@@ -1,26 +1,66 @@
 import { useRouter } from 'next/navigation';
 import React, { FC, useState } from 'react';
+import Image from 'next/image';
 
-interface NavUnloggedProps {
+interface NavUnloggedProps {}
 
-}
+const NavUnlogged: FC<NavUnloggedProps> = ({}) => {
+  const router = useRouter();
+  const handleHome = () => {
+    router.push('/');
+  };
+  const handleLogin = () => {
+    router.push('/pages/auth/login');
+  };
+  const [pageLanguage, setPageLanguage] = useState('ES');
+  const [selectOpen, setSelectOpen] = useState(false);
 
-const NavUnlogged: FC<NavUnloggedProps> = ({ }) => {
-  const router = useRouter()
-  const handleHome = ()=>{
-    router.push('/')
-  }
-  const handleLogin = ()=>{
-    router.push('/pages/auth/login')
-  }
-  const [pageLanguage, setPageLanguage] = useState("ES")
-  
+  const handleLanguageChange = (language: string) => {
+    setPageLanguage(language);
+    setSelectOpen(false);
+    setPageLanguage(language);
+  };
+
   return (
-    <nav className='flex flex-row justify-between p-5 items-center px-12'>
-      <div onClick={()=>{handleHome()}}> <p >Sharoku</p></div>
-      <div className='flex flex-row  '>
-        <div className='flex flex-row p-2' onClick={()=>{handleLogin()}}><p>Acceder</p></div>
-        <div className='flex flex-row p-2'><p>Idioma ( {pageLanguage} )</p></div>
+    <nav className="flex flex-row justify-between p-5 items-center px-12">
+      <div onClick={() => handleHome()}>
+        <p>Sharoku</p>
+      </div>
+      <div className="flex flex-row">
+        <div className="flex flex-row p-2" onClick={() => handleLogin()}>
+          <p>Acceder</p>
+        </div>
+        <div className="relative">
+          <div
+            className="flex flex-row p-2 items-center cursor-pointer"
+            onClick={() => setSelectOpen(!selectOpen)}
+          >
+            <p>Idioma ({pageLanguage})</p>
+            <Image
+              className="mx-2"
+              src={`/${pageLanguage}.png`}
+              height={10}
+              width={20}
+              alt=""
+            />
+          </div>
+          {selectOpen && (
+            <div className="absolute mt-2 bg-white border border-gray-300 rounded shadow-lg bg-gray-400 w-24 text-center">
+              <p
+                className=" p-2 hover:bg-gray-200"
+                onClick={() => handleLanguageChange('ES')}
+              >
+                ESPAÑOL
+              </p>
+              <p
+                className=" p-2 hover:bg-gray-200"
+                onClick={() => handleLanguageChange('EN')}
+              >
+                ENGLISH
+              </p>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
