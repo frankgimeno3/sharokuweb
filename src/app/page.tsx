e2f817efb5d605fc340a387f33ccbd8f63d3271a
frank gimeno <frankgimeno3@gmail.com>
2024-05-11 15:09:46 +0200

    añado redireccion de homepage a dashboard si no se está loggeado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,19 @@ import Mobile from "./components/unlogged/home/Mobile";
 import Web from "./components/unlogged/home/Web";
 import NavUnlogged from "./components/unlogged/nav/NavUnlogged";
 import Footer from "./components/general/Footer";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   const isMobile = useMediaQuery({ query: '(max-width: 768px)' }); 
 
+  
+  const { data: session, status } = useSession()
+
+  if (status === "authenticated") {
+    redirect('/pages/dashboard');
+  }
+  
   return (
     <div className="min-h-screen flex flex-col justify-between">
     <main className="flex flex-col   ">
