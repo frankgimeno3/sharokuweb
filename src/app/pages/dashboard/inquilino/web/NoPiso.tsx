f3eb30fbaa9621e31ccae70caac404973e78de29
frank gimeno <frankgimeno3@gmail.com>
2024-05-14 22:00:09 +0200

    le meto contenido a no piso

--- src/app/pages/dashboard/inquilino/web/NoPiso.tsx
+++ src/app/pages/dashboard/inquilino/web/NoPiso.tsx
@@ -8,7 +8,9 @@ const NoPiso: FC<NoPisoProps> = ({ }) => {
   return (
     <>
     <p>Actualmente no tienes o no has añadido tu piso o cuarto a sharoku</p>
-
+    <button>Busca un piso disponible para alquilar</button>
+    <button>Busca habitaciones en pisos compartidos</button>
+    <button>Ya tienes piso?</button>
 </>  );
 };
 
