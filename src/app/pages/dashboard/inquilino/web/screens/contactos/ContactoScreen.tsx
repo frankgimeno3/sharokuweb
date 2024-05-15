9526398ac8d85a53da46e2c67e67f0af2d82d8ab
frank gimeno <frankgimeno3@gmail.com>
2024-05-15 19:49:00 +0200

    añado contenido base a contactos

---
+++ src/app/pages/dashboard/inquilino/web/screens/contactos/ContactoScreen.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+
+interface ContactoScreenProps {
+  
+}
+
+const ContactoScreen: FC<ContactoScreenProps> = ({ }) => {
+  return (
+    <div className='flex-col justify-center items-center text-center' >
+    <p className='font-bold py-12 text-3xl '>Mis contactos</p>
+ 
+    <div className='flex flex-col mx-5'>
+            <button className='bg-white rounded-lg shadow  m-5 py-5'>Favoritos</button>
+            <button className='bg-white rounded-lg shadow  m-5 py-5'>Chat del piso</button>
+            <button className='bg-white rounded-lg shadow  m-5 py-5'>Usuarios</button>
+            <button className='bg-white rounded-lg shadow  m-5 py-5'>Propietarios y administradores</button>
+            <button className='bg-white rounded-lg shadow  m-5 py-5'>Proveedores de Servicios</button>
+     </div>
+    </div>
+  );
+};
+
+export default ContactoScreen;
